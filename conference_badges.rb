45eb0792b98ae8a1b0ca705a153d02e0df9f20d6
Alyssa Brown <alyssa.brown@live.com>
2019-01-22 03:12:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,10 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end 
 
-def batch_badge_creator(array)
+def batch_badge_creator(name)
   new_array=[]
+  badge_maker(name).each{|name|new_array}
+end 
   
   
   
